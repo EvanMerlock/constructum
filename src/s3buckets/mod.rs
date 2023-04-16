5149fe325ca313920dd44c098c6bbaa460c718cd
Evan <evan@merlock.dev>
2023-04-16 10:54:12 -0500

    fix clippy lint in s3bucket

--- src/s3buckets/mod.rs
+++ src/s3buckets/mod.rs
@@ -2,7 +2,6 @@ use s3::Bucket;
 
 pub async fn get_file_from_s3(file_name: String, s3_bucket: Bucket) -> Result<String, s3::error::S3Error> {
     let obj = s3_bucket.get_object(file_name).await?;
-    // TODO: inefficient; extra copying
-    let result = String::from_utf8(obj.bytes().iter().copied().collect()).expect("failed to convert to utf8");
+    let result = String::from_utf8(obj.bytes().to_vec()).expect("failed to convert to utf8");
     Ok(result)
 }
